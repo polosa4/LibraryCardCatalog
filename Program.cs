17eedd6a5b4e3b563af1b9343ff9539aabe89209
polosa4 <polosa4@gmail.com>
2017-04-20 09:42:55 -0500

    CardCatalog

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCardCatalog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name of the file: ");
+            var userFilename = Console.ReadLine();
+            if userFilename !== null {
+                Console.WriteLine()
+            }
+
+        }
+    }
+}
